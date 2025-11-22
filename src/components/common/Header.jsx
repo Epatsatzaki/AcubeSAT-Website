bac7d1017236ca9f415afd154e5d4383b197b5ce
E.patsatzaki <evapatsatzakis@gmail.com>
2025-11-22 22:21:02 +0200

    added acubesat logo in navbar

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,12 +1,18 @@
 import Navbar from './Navbar'
-
+// import "../../../public/images/SpaceDot-logos/AcubeSAT"
 const Header = () => {
   return (
     <header className="bg-white dark:bg-gray-800 sticky top-0 z-50 shadow-sm">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
-          <div className="flex items-center">
-            <a href="/" className="text-2xl font-bold text-primary-600">SpaceDot</a>
+          <div className="flex items-center space-x-3"> {/* Added space-x-3 for gap */}
+            {/* Logo */}
+            <img 
+              src="images/SpaceDot-logos/AcubeSAT.png" 
+              className="h-[65px] w-[65px]" 
+            />
+            {/* Brand Name */}
+            <a href="/" className="text-2xl font-bold text-primary-600">AcubeSAT</a>
           </div>
           <Navbar />
         </div>
