377dde634dd49d5d1887450e8e51a706fac04d60
E.patsatzaki <evapatsatzakis@gmail.com>
2025-12-06 17:41:03 +0200

    update footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,3 +1,5 @@
+import * as simpleIcons from "simple-icons";
+
 const Footer = () => {
   // Define your logo data here
   const footerLogos = [
@@ -7,14 +9,13 @@ const Footer = () => {
     { name: "SpaceDot", link: "https://spacedot.gr", image: "location of spacedot logo" },
   ];
 
-  // Social media data
-  const socialMedia = [
-    { name: "Facebook", link: "https://facebook.com/yourpage", icon: "M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" },
-    { name: "Instagram", link: "https://instagram.com/yourpage", icon: "M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" },
-    { name: "LinkedIn", link: "https://linkedin.com/company/yourcompany", icon: "M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 01-2.063-2.065 2.064 2.064 0 112.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" },
-    { name: "YouTube", link: "https://youtube.com/yourchannel", icon: "M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" },
-    { name: "GitLab", link: "https://youtube.com/yourchannel", icon: "M23.955 13.587l-1.342-4.135-2.664-8.189c-.135-.423-.73-.423-.867 0l-2.664 8.189H7.044L4.38 1.263C4.242.84 3.65.84 3.513 1.263L.845 9.452.001 13.587c-.161.499.136 1.033.636 1.195l11.393 4.004a.72.72 0 00.236.042.72.72 0 00.236-.042l11.393-4.004c.5-.162.798-.696.636-1.195zm-2.332 2.031l-9.8 3.448-9.8-3.448.647-2.015 2.537-7.902h4.835l2.232 6.923a.724.724 0 001.354 0l2.232-6.923h4.835l2.537 7.902.647 2.015z" },
-  ];
+const socialMedia = [
+  { name: "Facebook", link: "...", icon: simpleIcons.siFacebook },
+  { name: "Instagram", link: "...", icon: simpleIcons.siInstagram },
+  { name: "LinkedIn", link: "...", icon: simpleIcons.siLinkedin },
+  { name: "YouTube", link: "...", icon: simpleIcons.siYoutube },
+  { name: "GitLab", link: "...", icon: simpleIcons.siGitlab },
+];
 
   return (
     <footer className="bg-gray-800 text-white">
@@ -25,7 +26,7 @@ const Footer = () => {
           <div className="mb-6 md:mb-0">
             <a 
               className="text-xl font-semibold hover:text-white no-underline text-gray-300 p-2 block" 
-              href="https://acubesat.spacedot.gr" // Replace with your actual AcubeSAT link
+              href="https://acubesat.spacedot.gr"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -59,7 +60,7 @@ const Footer = () => {
         </div>
 
         {/* Logo section - 4 logos in a row */}
-        <div className="flex flex-wrap justify-center items-center border-t border-gray-700 pt-8 gap-8">
+        <div className="flex flex-wrap justify-center items-center gap-8">
           {footerLogos.map((logo, index) => (
             <a 
               key={index}
@@ -99,7 +100,7 @@ const Footer = () => {
                 aria-label={social.name}
               >
                 <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-                  <path d={social.icon} />
+                  <path d={social.icon.path} />
                 </svg>
               </a>
             ))}
