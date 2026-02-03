0362015d84c6dbccae3257e1c15a499ec4242229
Evangelia <evapatsatzakis@gmail.com>
2026-02-03 23:31:47 +0200

    changes to make the page lighter

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { compression } from 'vite-plugin-compression2' // Import the plugin
 
 export default defineConfig(({ command }) => {
   return {
     base: command === 'build' ? '/AcubeSAT-Website/' : '/',
-    plugins: [react()],
+    plugins: [
+      react(),
+      // This generates .br (Brotli) files
+      compression({
+        algorithm: 'brotliCompress',
+        exclude: [/\.(br)$/, /\.(gz)$/],
+      }),
+      // This generates .gz (Gzip) files as a backup
+      compression({
+        algorithm: 'gzip',
+        exclude: [/\.(br)$/, /\.(gz)$/],
+      }),
+    ],
     server: {
       port: 3000,
     },
